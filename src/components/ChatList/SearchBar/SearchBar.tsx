26cec52fb05c506df7c4eb0069e015e153b6e592
Matt Miller <matt@m11r.dev>
2022-08-04 13:39:57 +0100

    Working on search bar layout

--- src/components/ChatList/SearchBar/SearchBar.tsx
+++ src/components/ChatList/SearchBar/SearchBar.tsx
@@ -1,29 +1,55 @@
 import clsx, { ClassValue } from "clsx";
-import React from "react";
-
+import React, { useState } from "react";
+import {
+  SortAscendingIcon,
+  SortDescendingIcon,
+  SearchIcon,
+} from "@heroicons/react/solid";
 export type Props = {
   children?: React.ReactNode;
   className?: ClassValue;
 };
 
 const SearchBar: React.FC<Props> = (props: Props) => {
+  const [ascending, setAscending] = useState(false);
+
   return (
-    <div className="px-4 py-2 flex flex-row">
+    <div className="px-4 py-2 flex flex-row grow">
       {/* TODO: Add functionality later */}
-      <div className="flex bg-default rounded-md p-1">
-        <span>
+      <div className="flex grow bg-default rounded-md p-1">
+        <span className="grow-0 pr-4">
           {/* icon that changes based on state */}
-          icon
+          <SearchIcon
+            className=" text-lg text-gray-400 hover:text-white"
+            width="15px"
+            height="15px"
+          />
         </span>
         <input
-          className="bg-default border-none"
+          className="bg-default grow-1 py-2 focus-visible:outline-dashed focus-visible:outline-1 focus-visible:outline-grey-400"
           type="text"
           name="search-query"
           id="SearchQuery"
           placeholder="Search or start new chat"
         />
       </div>
-      <span>icon</span>
+      <span className="grow-0 pl-2">
+        {ascending ? (
+          <SortDescendingIcon
+            onClick={() => setAscending(!ascending)}
+            className=" text-lg text-gray-400 hover:text-white"
+            width="24px"
+            height="24px"
+          />
+        ) : (
+          <SortAscendingIcon
+            onClick={() => setAscending(!ascending)}
+            className="text-lg text-gray-400 hover:text-white"
+            width="24px"
+            height="24px"
+          />
+        )}
+      </span>
     </div>
   );
 };
