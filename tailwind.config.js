a53eb4930e339b3474d57ffd3231256bd07985db
Matt Miller <matt@m11r.dev>
2022-08-15 13:32:51 +0100

    Gradually creating our colour system

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,18 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        'light-grey': '#2a3942',
-        'mid-grey': '#202c33',
-        'dark-grey': '#111b21',
-        'midnight-black': '#0b141a',
-        'dead-white': '#e9edef'
-      },
+    colors: {
+      "light-grey": "#dfdfdf",
+      "mid-grey": "#33383b",
+      "dark-grey": "#1f242a",
+      "blue-grey": "#2a3942",
+      secondary: "#8696a0",
+      "midnight-black": "#0b141a",
+      "dead-white": "#e9edef",
+      "background-chat": "#10181b",
+      "background-chat-list": "#131c23",
     },
+    extend: {},
   },
   plugins: [],
 };
