c711d2cf9bbe01ad8782d7e2c8902bbac41d09a5
Matt Miller <matt@m11r.dev>
2022-08-31 11:13:58 +0100

    Tidying post update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      "light-grey": "#dfdfdf",
+      "mid-grey": "#33383b",
+      "dark-grey": "#1f242a",
+      "blue-grey": "#2a3942",
+      secondary: "#8696a0",
+      "midnight-black": "#0b141a",
+      "dead-white": "#e9edef",
+      "background-chat": "#10181b",
+      "background-chat-list": "#131c23",
+    },
     extend: {},
   },
   plugins: [],
